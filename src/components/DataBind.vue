8b12fd8c24db7052f32365251ff64be0b67093a1
IBMRupak <ibmrupak11@gmail.com>
2021-10-12 20:24:39 +0530

    Data Binding

--- src/components/DataBind.vue
+++ src/components/DataBind.vue
@@ -3,10 +3,14 @@
 
         <h1>Data Bind</h1>
 
-        <h1>{{message}}</h1> 
+
+<!-- One way Data Binding -->
+        <h1 style="white-space:pre-line">{{message}}</h1> 
          <!-- view part --> 
          <!-- Data binding -->
-      <input type= "text" v-model="message">           
+      <!-- <input type= "text" v-model="message">            -->
+
+      <textarea type="text" v-model="message"/>
 
     </div>
 </template>
